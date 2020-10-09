5690b83c5b08d70b16ea60c33fb716fcdd6f4069
Solst1tium <solange.leiva@mayor.cl>
2020-10-08 21:36:43 -0300

    vista playlist

--- src/views/Playlist.vue
+++ src/views/Playlist.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-    
-  </div>
+  <v-parallax
+    dark
+    src="https://hipertextual.com/files/2017/03/background_blemishes_dark_91678_2560x1600.jpg"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        <h1 class="display-1 font-weight-thin mb-4">
+          No has ingresado ningún album a tu playlist
+        </h1>
+        <h4 class="subheading">
+          Explora y arma tu albúm
+        </h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
-
-<style>
-
-</style>
